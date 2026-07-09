66fa96b524b7122096976a57314243a4eb401f6e
sas-deviq-iq <sas-deviq-iq@users.noreply.github.com>
2026-07-09 22:11:21 +0300

    Fix: Prevent double proxying of URLs which broke image loading

--- app/src/main/java/com/example/tubetogether/api/ImageProxy.kt
+++ app/src/main/java/com/example/tubetogether/api/ImageProxy.kt
@@ -9,6 +9,9 @@ object ImageProxy {
         if (url.startsWith("//")) url = "https:$url"
         
         val proxyBase = CryptoUtil.decrypt("RRd4PRdMI3wYWyJ/H1MifB9TIn8dVzZ1HVs8YkwTZWJJGmIsQApvclkCfipIFzE=")
+        if (url.startsWith(proxyBase)) {
+            return url
+        }
         return try {
             proxyBase + java.net.URLEncoder.encode(url, "UTF-8")
         } catch (e: Exception) {
