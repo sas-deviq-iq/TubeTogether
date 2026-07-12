7cb8e4df5c371581ca8c98548469a8bdc0b7f364
sas-deviq-iq <sas-deviq-iq@users.noreply.github.com>
2026-07-12 16:48:15 +0300

    Bump version to 1.3.0 and update OTA config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.tubetogether"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
